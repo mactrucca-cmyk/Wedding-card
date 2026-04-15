9c663b4b78f0d01531a253e19cf7f5229b2432ef
mactrucca-cmyk <mactrucca@gmail.com>
2026-04-15 14:13:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,9 @@ const CONFIG = {
   ],
 
   gallery: [
-    { src: "images/BEN_0610.jpg", cap: "Cô dâu xinh đẹp" },
+        { src: "images/01.jpg",       cap: "Ngày trọng đại" },
+     { src: "images/02.jpg",       cap: "Ngày trọng đại" },
+     { src: "images/BEN_0610.jpg", cap: "Cô dâu xinh đẹp" },
     { src: "images/BEN_0690.jpg", cap: "Chú rể lịch lãm" },
     { src: "images/BEN_9264.jpg",       cap: "Ngày trọng đại" },
     { src: "images/BEN_9164.jpg",       cap: "Ngày trọng đại" },
@@ -116,7 +118,9 @@ const CONFIG = {
   ],
 
   albumImages: [
-    { src: "images/BEN_0610.jpg", cat: "anh-cuoi", cap: "Ảnh cưới đẹp" },
+    { src: "images/01.jpg",       cat: "dinh-hon", cap: "Ảnh cưới đẹp" },
+     { src: "images/02.jpg",       cat: "dinh-hon", cap: "Ảnh cưới đẹp" },    
+     { src: "images/BEN_0610.jpg", cat: "anh-cuoi", cap: "Ảnh cưới đẹp" },
     { src: "images/BEN_0690.jpg", cat: "anh-cuoi", cap: "Ảnh cưới đẹp" },
     { src: "images/03.jpg",       cat: "dinh-hon", cap: "Ảnh cưới đẹp" },
     { src: "images/BEN_0232.jpg",       cat: "anh-cuoi", cap: "Ảnh cưới đẹp" },
